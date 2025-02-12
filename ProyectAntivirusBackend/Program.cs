15210dadd9277e2d6e57900788cd8f379318519d
Cuoralex <alexacuartas@gmail.com>
2025-02-11 20:56:20 -0500

    Creación rama DevCuoralex

--- ProyectAntivirusBackend/Program.cs
+++ ProyectAntivirusBackend/Program.cs
@@ -1,4 +1,16 @@
+using Supabase;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var supabaseUrl = builder.Configuration["Supabase:Url"];
+var supabaseKey = builder.Configuration["Supabase:ApiKey"];
+
+var options = new SupabaseOptions { AutoConnectRealtime = true };
+var supabase = new Client(supabaseUrl, supabaseKey, options);
+
+// Add Supabase
+builder.Services.AddSingleton(supabase);
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
