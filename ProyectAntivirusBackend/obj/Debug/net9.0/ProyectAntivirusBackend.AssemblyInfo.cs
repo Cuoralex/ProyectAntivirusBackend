257d18c031f81fcdd25154dff480923d5d581846
Cuoralex <alexacuartas@gmail.com>
2025-02-16 14:27:25 -0500

    Actualización de la rama

--- ProyectAntivirusBackend/obj/Debug/net9.0/ProyectAntivirusBackend.AssemblyInfo.cs
+++ ProyectAntivirusBackend/obj/Debug/net9.0/ProyectAntivirusBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectAntivirusBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ffc59e946457b1a59282584c024b600a10d1c21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+801d14a3bcbe8cf02e45eaa82adf543f4ab1c20e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectAntivirusBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectAntivirusBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
